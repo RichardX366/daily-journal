14712003110839e96268ad2949e8998d956a20bf
Richard <richardx366@gmail.com>
2023-08-05 00:16:41 -0400

    useeffect check

--- src/pages/entry/[id].tsx
+++ src/pages/entry/[id].tsx
@@ -1,9 +1,13 @@
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Entry: React.FC = () => {
   const router = useRouter();
   const { id } = router.query;
+
+  useEffect(() => {
+    console.log(id);
+  }, [id]);
 
   return <div>Entry {id}</div>;
 };
